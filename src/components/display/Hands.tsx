6d85bd24dc4c94be8af0fdcb26388e0804bfe76f
KazukiAmatatsu <80532711+KazukiAmatatsu@users.noreply.github.com>
2021-07-25 18:14:07 +0900

    修正

--- src/components/display/Hands.tsx
+++ src/components/display/Hands.tsx
@@ -5,44 +5,23 @@ const Hands = () => {
   const roomInfo = useRecoilValue(room)
   const userInfo = useRecoilValue(user)
   const member = roomInfo.member
-  // const userId =
-  //   member &&
-  //   Object.keys(member).map((id) => {
-  //     if (id === userInfo.id) {
-  //       return id
-  //     } else {
-  //       return null
-  //     }
-  //   })
-  // console.log(userId)
-
-  // const card =
-  //   member &&
-  //   Object.values(member).map((id) => {
-  //     if (id.name === userInfo.name) {
-  //       return id.hand
-  //     } else {
-  //       return null
-  //     }
-  //   })
 
   return (
     <>
-      {userInfo.name}
       {member &&
         Object.entries(member).map(([id, data]) => {
           if (id === userInfo.id) {
             return (
               <>
-                <h3>{data.name}</h3>
                 <div style={{ display: 'none' }}>{data.hand}</div>
+                <h3>{data.name}</h3>
               </>
             )
           } else {
             return (
               <>
-                <h3>{data.name}</h3>
                 <div>{data.hand}</div>
+                <h3>{data.name}</h3>
               </>
             )
           }
