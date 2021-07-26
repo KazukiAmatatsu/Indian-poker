cfed2b6fa60f2c684635870f63c53d682b9dc195
KazukiAmatatsu <80532711+KazukiAmatatsu@users.noreply.github.com>
2021-07-27 00:47:51 +0900

    全員がReadyしたらゲームスタートできる

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,6 +1,5 @@
 import Top from './Top'
 import Standby from './Standby'
 import Room from './Room'
-import Game from './Game'
 
-export { Top, Standby, Room, Game }
+export { Top, Standby, Room }
--- src/components/display/Member.tsx
+++ src/components/display/Member.tsx
@@ -4,14 +4,15 @@ import { useRecoilValue } from 'recoil'
 export const Member = () => {
   const roomInfo = useRecoilValue(room)
   const member = roomInfo.member
-  // const mem = member && Object.values(member)
-  // console.log(mem)
   return (
     <>
-      {member &&
-        Object.values(member).map((player, index) => (
-          <li key={index}>{player.name}</li>
-        ))}
+      <h2>メンバー</h2>
+      <ul>
+        {member &&
+          Object.values(member).map((player, index) => (
+            <li key={index}>{player.name}</li>
+          ))}
+      </ul>
     </>
   )
 }
